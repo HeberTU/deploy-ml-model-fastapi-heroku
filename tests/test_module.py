3b03fbf082e26b922ceed3677f4f9859fb8320ac
Heber Trujillo <heber.trj.urt@gmail.com>
2022-04-17 11:27:44 +0200

    [UPD] New patch version

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,11 +2,11 @@
 """Module Test.
 
 Created on: 4/17/2022
-@author: Heber Trujillo <heber.trj.urt@gmail.com> 
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
 from src import __version__
 
 
 def test_version():
-    assert __version__=="0.0.1"
+    assert __version__ == "0.0.2"

df242f69f9bb37f59dec2aa5e3cacadad7e52e7d
Heber Trujillo <heber.trj.urt@gmail.com>
2022-04-17 10:42:14 +0200

    [NEW] pytest suit init.

---
+++ tests/test_module.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Module Test.
+
+Created on: 4/17/2022
+@author: Heber Trujillo <heber.trj.urt@gmail.com> 
+Licence,
+"""
+from src import __version__
+
+
+def test_version():
+    assert __version__=="0.0.1"

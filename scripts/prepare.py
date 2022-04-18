23713cb18f7ac3a9babd487f5371bc033e7ffc4a
Heber Trujillo <heber.trj.urt@gmail.com>
2022-04-18 18:16:51 +0200

    [UPD] lining prepare.py script.

--- scripts/prepare.py
+++ scripts/prepare.py
@@ -5,21 +5,19 @@ Created on: 4/18/2022
 @author: Heber Trujillo <heber.trj.urt@gmail.com> 
 Licence,
 """
+from src.ml.data import data_config, preprocess_data
 from src.settings import settings
-from src.ml.data import preprocess_data, data_config
 
 
 def main(dry: bool = False):
     """Preprocess script."""
-
     census_data = preprocess_data()
 
     if not dry:
         census_data.to_csv(
-            path_or_buf=settings.DATA_PATH / data_config["clean"]["name"],
-            index=False
+            path_or_buf=settings.DATA_PATH / data_config["clean"]["name"], index=False
         )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
